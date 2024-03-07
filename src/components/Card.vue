5d6d318fdea675b80d8ae92169c31dcdc6942e1c
Poom <papangkorn.poomm@gmail.com>
2024-03-07 17:13:40 +0700

    [Update] Ui Card.vue

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,16 +1,91 @@
-<script setup>
+<script setup></script>
 
-</script>
- 
 <template>
-    <div class="flex flex-col">
-        <h1>This is card page !!!</h1>
-        <router-link to="/statistics"><button>statistics</button></router-link>
-        <router-link to="/details"><button>details</button></router-link>
-        <router-link to="/"><button>home</button></router-link>
-    </div>
-</template>
- 
-<style scoped>
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
+  />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
+  />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
+  />
+  <!-- <div class="flex justify-between w-full h-24 bg-red-300">
+    <h1>This is card page !!!</h1>
+    <router-link to="/statistics"><button>statistics</button></router-link>
+    <router-link to="/details"><button>details</button></router-link>
+    <router-link to="/"><button>home</button></router-link>
+  </div> -->
 
-</style>
+  <div class="flex justify-center items-center h-screen">
+    <div class="w-[22vw] h-[70vh] bg-slate-600 rounded-md pt-5">
+      <div class="flex items-center justify-center gap-x-5">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqogG_tIinH_K1HoXM24xH5KC2hrmNs2BpX2PCmXGCgUUlVpIwBiSQMbJU13DAqRTIj2g&usqp=CAU"
+          class="size-36 rounded-full p-2"
+        />
+
+        <div class="flex flex-col h-1/3 items">
+          <h1 class="text-2xl font-bold text-black">User test</h1>
+          <div class="indent-4">
+            <h3 class="text-xs font-medium text-black">
+              Age <slot name="age"></slot>
+            </h3>
+            <h3 class="text-xs font-medium text-black">
+              Position <slot name="position"></slot>
+            </h3>
+            <h3 class="text-xs font-medium text-black">
+              Personality <slot name="personality"></slot>
+            </h3>
+          </div>
+        </div>
+      </div>
+
+      <div class="flex flex-col gap-y-2">
+        <h2 class="text-lg font-medium text-black underline ml-2">
+          Pain Point
+        </h2>
+        <ul class="indent-2">
+          <li>Pain Point<slot name="painPoint"></slot></li>
+        </ul>
+
+        <h3 class="text-lg font-medium text-black underline ml-2">
+          Goal and Needs
+        </h3>
+        <ul class="indent-2">
+          <li>Goal and Needs<slot name="GoalAndNeeds"></slot></li>
+        </ul>
+
+        <h3 class="text-lg font-medium text-black underline ml-2">Rating</h3>
+        <div class="flex justify-center gap-x-10">
+          <div
+            class="radial-progress"
+            style="--value: 10; --size: 4rem"
+            role="progressbar"
+          >
+            <span class="material-symbols-outlined"> groups </span>
+          </div>
+          <div
+            class="radial-progress"
+            style="--value: 20; --size: 4rem"
+            role="progressbar"
+          >
+            <span class="material-symbols-outlined"> source_environment </span>
+          </div>
+          <div
+            class="radial-progress"
+            style="--value: 90; --size: 4rem"
+            role="progressbar"
+          >
+            <span class="material-symbols-outlined"> work </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,15 @@ import { createRouter,createWebHistory } from "vue-router";
 import  Home  from "../components/Home.vue";
 import  Details  from "../components/Details.vue";
 import  Statistics  from "../components/Statistics.vue";
+import Card from "../components/Card.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes:[
         {path:"/", name:"Home",component: Home},
         {path:"/details", name:"Details",component: Details},
-        {path:"/statistics", name:"statistics",component: Statistics}
+        {path:"/statistics", name:"statistics",component: Statistics},
+        {path:"/card", name:"card",component: Card},
     ]
 })
 
