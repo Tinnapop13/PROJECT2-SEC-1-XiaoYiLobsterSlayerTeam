8fc5b3fd4747ae3f26f693f2aff5a2bd76078a91
Poom <papangkorn.poomm@gmail.com>
2024-03-14 16:34:43 +0700

    [Update] HomeUi & CardUi

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -14,75 +14,69 @@
     href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
   />
 
-  <!-- <div class="flex justify-between w-full h-24 bg-red-300">
-    <h1>This is card page !!!</h1>
-    <router-link to="/statistics"><button>statistics</button></router-link>
-    <router-link to="/details"><button>details</button></router-link>
-    <router-link to="/"><button>home</button></router-link>
-  </div> -->
+  <div
+    class="w-2/4 bg-[#F9F7F7] rounded-xl py-5 px-5 max-sm:w-4/5 max-sm:h-5/6"
+  >
+    <div class="flex items-center justify-center gap-x-5 mb-3">
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqogG_tIinH_K1HoXM24xH5KC2hrmNs2BpX2PCmXGCgUUlVpIwBiSQMbJU13DAqRTIj2g&usqp=CAU"
+        class="relative size-32 rounded-full"
+      />
 
-  <div class="flex justify-center items-center min-h-screen">
-    <div class="w-1/5 h-[70vh] bg-slate-600 rounded-md pt-5 max-sm:w-3/4">
-      <div class="flex items-center justify-center gap-x-5">
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqogG_tIinH_K1HoXM24xH5KC2hrmNs2BpX2PCmXGCgUUlVpIwBiSQMbJU13DAqRTIj2g&usqp=CAU"
-          class="size-32 rounded-full p-2"
-        />
-
-        <div class="flex flex-col h-1/3">
-          <h1 class="text-2xl font-bold text-black">User test</h1>
-          <div class="ml-2">
-            <h3 class="text-xs font-medium text-black">
-              Age <slot name="age"></slot>
-            </h3>
-            <h3 class="text-xs font-medium text-black">
-              Position <slot name="position"></slot>
-            </h3>
-            <h3 class="text-xs font-medium text-black">
-              Personality <slot name="personality"></slot>
-            </h3>
-          </div>
+      <div class="flex flex-col h-1/3">
+        <h1 class="text-2xl font-bold text-[#3F72AF]">
+          <slot name="fullname"></slot>
+        </h1>
+        <div class="indent-3">
+          <h3 class="text-xs font-medium text-[#3F72AF]">
+            Age : <slot name="age"></slot>
+          </h3>
+          <h3 class="text-xs font-medium text-[#3F72AF]">
+            Position : <slot name="position"></slot>
+          </h3>
+          <h3 class="text-xs font-medium text-[#3F72AF]">
+            Comment : <slot name="comment"></slot>
+          </h3>
         </div>
       </div>
+    </div>
 
-      <div class="flex flex-col gap-y-2">
-        <h2 class="text-lg font-medium text-black underline ml-2">
-          Pain Point
-        </h2>
-        <ul class="ml-8 list-disc">
-          <li>Pain Point<slot name="painPoint"></slot></li>
-        </ul>
+    <div class="flex flex-col">
+      <h2 class="text-lg font-extrabold text-[#3F72AF] underline">
+        Pain Points
+      </h2>
 
-        <h3 class="text-lg font-medium text-black underline ml-2">
-          Goal and Needs
-        </h3>
-        <ul class="ml-8 list-disc">
-          <li>Goal and Needs<slot name="GoalAndNeeds"></slot></li>
-        </ul>
+      <p class="text-[#3F72AF]">
+        <slot name="PainPoints"></slot>
+      </p>
 
-        <h3 class="text-lg font-medium text-black underline ml-2">Rating</h3>
-        <div class="flex justify-center justify-evenly">
-          <div
-            class="radial-progress"
-            style="--value: 10; --size: 4rem"
-            role="progressbar"
-          >
-            <span class="material-symbols-outlined"> groups </span>
-          </div>
-          <div
-            class="radial-progress"
-            style="--value: 20; --size: 4rem"
-            role="progressbar"
-          >
-            <span class="material-symbols-outlined"> source_environment </span>
-          </div>
-          <div
-            class="radial-progress"
-            style="--value: 90; --size: 4rem"
-            role="progressbar"
-          >
-            <span class="material-symbols-outlined"> work </span>
-          </div>
+      <h3 class="text-[#3F72AF] underline">Goal and Needs</h3>
+
+      <p class="text-[#3F72AF]">
+        <slot name="GoalAndNeeds"></slot>
+      </p>
+
+      <div class="">
+        <div
+          class="radial-progress text-[#9fb4dd]"
+          style="--value: 20; --size: 2rem"
+          role="progressbar"
+        >
+          <span class="material-symbols-outlined"> groups </span>
+        </div>
+        <div
+          class="radial-progress text-[#9fb4dd]"
+          style="--value: 60; --size: 2rem"
+          role="progressbar"
+        >
+          <span class="material-symbols-outlined"> source_environment </span>
+        </div>
+        <div
+          class="radial-progress text-[#9fb4dd]"
+          style="--value: 80; --size: 2rem"
+          role="progressbar"
+        >
+          <span class="material-symbols-outlined"> work </span>
         </div>
       </div>
     </div>
