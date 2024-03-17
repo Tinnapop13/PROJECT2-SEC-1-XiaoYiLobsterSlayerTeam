1b4a0b7dbbe93336ad37096a1c5cca5a6288a974
Poom <papangkorn.poomm@gmail.com>
2024-03-17 17:14:37 +0700

    [Update] Card tailwind

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,7 @@
 <script setup>
 const props = defineProps(["deleteCard"])
-console.log(props.deleteCard);
+console.log(props.deleteCard)
+
 </script>
 
 <template>
@@ -18,19 +19,21 @@ console.log(props.deleteCard);
   />
 
   <!-- User Card -->
-  <div class="w-[20rem] h-[30rem] bg-[#F9F7F7] rounded-xl">
+  <div class="w-[20rem] h-[35rem] bg-[#F9F7F7] rounded-xl max-sm:w-full  ">
     <!-- image and details -->
-    <div class="flex items-center justify-center h-[10rem] gap-5">
+    <div
+      class="flex items-center justify-center mx-auto h-[10rem] gap-x-5 bg-blue-300 rounded-b-xl"
+    >
       <img
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqogG_tIinH_K1HoXM24xH5KC2hrmNs2BpX2PCmXGCgUUlVpIwBiSQMbJU13DAqRTIj2g&usqp=CAU"
         class="size-24 rounded-full"
       />
 
-      <!-- div details -->
-
-      <!-- Age and Position -->
-      <div class="flex flex-col justify-center">
-        <h1 class="text-xl font-extrabold text-[#3F72AF]">
+      <!-- div details : name, age and position -->
+      <div
+        class="flex flex-col justify-center bg-[#F9F7F7] w-[20vh] max-h-[18vh] rounded-bl-2xl rounded-tr-2xl p-3 resize-none"
+      >
+        <h1 class="text-xl font-extrabold text-[#3F72AF] break-words">
           <slot name="fullname"></slot>
         </h1>
 
@@ -43,17 +46,17 @@ console.log(props.deleteCard);
         <h3 class="font-bold text-sm text-[#3F72AF]">
           Position:
           <span class="text-base font-medium text-[#3e608a]"
-            ><slot name="position"></slot></span
-          >
+            ><slot name="position"></slot
+          ></span>
         </h3>
       </div>
     </div>
 
-    <!-- Comment -->
-    <div class="flex flex-col mx-5">
+    <!-- Comment, PainPoints, GoalsAndNeeds -->
+    <div class="flex flex-col mx-5 my-2 max-h-[30rem]">
+      <!-- Comment -->
       <h2 class="text-lg font-extrabold text-[#3F72AF] underline">Comment</h2>
-
-      <p class="text-[#3F72AF]">
+      <p class="text-[#3F72AF] break-words text-sm">
         <slot name="comment"></slot>
       </p>
 
@@ -61,15 +64,13 @@ console.log(props.deleteCard);
       <h2 class="text-lg font-extrabold text-[#3F72AF] underline">
         Pain Points
       </h2>
-
-      <p class="text-[#3F72AF]">
+      <p class="text-[#3F72AF] break-words text-sm">
         <slot name="PainPoints"></slot>
       </p>
 
       <!-- GoalAndNeed -->
       <h2 class="text-[#3F72AF] font-extrabold underline">Goal and Needs</h2>
-
-      <p class="text-[#3F72AF]">
+      <p class="text-[#3F72AF] break-words text-sm">
         <slot name="GoalAndNeeds"></slot>
       </p>
 
