89d6ed4b330bb5901cd2a8ab9ac367f3e5dabbd2
Poom <papangkorn.poomm@gmail.com>
2024-03-25 20:04:46 +0700

    [Fix&Update] detailsUI, AddCard redirect to Home

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,28 @@
-import { createRouter,createWebHistory } from "vue-router";
-import  Home  from "../components/Home.vue";
-import  Details  from "../components/Details.vue";
-import Card from "../components/Card.vue";
-import AddCard from "@/components/AddCard.vue";
-import Authentication from "@/components/Authentication.vue";
-import { authenticationStore } from "@/stores/authenticationStore.js"
+import {createRouter, createWebHistory} from "vue-router"
+import Home from "../components/Home.vue"
+import Details from "../components/Details.vue"
+import Card from "../components/Card.vue"
+import AddCard from "@/components/AddCard.vue"
+import Authentication from "@/components/Authentication.vue"
+import {authenticationStore} from "@/stores/authenticationStore.js"
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes:[
-        {path:"/home", name:"Home",component: Home},
-        {path:"/details/:id", name:"Details",component: Details},
-        {path:"/card", name:"card",component: Card},
-        {path:"/addcard", name:"addcard",component: AddCard },
-        {path:"/" , name:"auth",component: Authentication },
-    ]
+  history: createWebHistory(),
+  routes: [
+    {path: "/home", name: "Home", component: Home},
+    {path: "/details/:id", name: "Details", component: Details},
+    {path: "/card", name: "card", component: Card},
+    {path: "/addcard", name: "addcard", component: AddCard},
+    {path: "/", name: "auth", component: Authentication},
+  ],
 })
 
 router.beforeEach(async (to, from) => {
   const authStore = authenticationStore()
-    if (!authStore.loggedIn && to.name !== 'auth') {
-      alert("Please Login First")
-      return { name: 'auth' }
-    }
-  })
+  if (!authStore.loggedIn && to.name !== "auth") {
+    alert("Please Login First")
+    return {name: "auth"}
+  }
+})
 
 export default router
