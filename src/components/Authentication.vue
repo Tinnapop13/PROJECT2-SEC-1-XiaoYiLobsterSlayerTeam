e57e116e18e2c8f80e3daaed777271d5d001a095
Natsaran Sae-oueng <zhinobid@gmail.com>
2024-03-26 09:52:07 +0700

    [Update] add Validate sign up in Authentication

--- src/components/Authentication.vue
+++ src/components/Authentication.vue
@@ -2,7 +2,7 @@
 import { UserManagement } from '@/libs/UserManagement.js'
 import { getUsersData , addUserData } from '@/libs/crud.js'
 import {useRouter} from "vue-router"
-import { ref , onMounted } from 'vue'
+import { ref , onMounted, computed } from 'vue'
 import Modal from '@/components/Modal.vue'
 import { errorStore } from '@/stores/errorStore'
 import { authenticationStore } from '@/stores/authenticationStore'
@@ -16,6 +16,10 @@ const route = useRouter()
 const username = ref('')
 const password = ref('')
 const validatePassword = ref('')
+const hasUpper = ref(false)
+const hasLower = ref(false)
+const hasDigit = ref(false)
+const hasSpecial = ref(false)
 
 const login = (event)=>{
     for (const [key,value] of Object.entries(userManager.value.getUsers())) {
@@ -43,6 +47,31 @@ const regist = (event)=>{
             return
         }
     } 
+    if(!/[0-9]/.test(password.value)){
+        errStore.loginErrorLog = "Password doesn't have any digit"
+        errStore.loginFailed = true
+        event.preventDefault();
+        return
+    }
+    if(!/[a-z]/.test(password.value)){
+        errStore.loginErrorLog = "Password doesn't have any lower case"
+        errStore.loginFailed = true
+        event.preventDefault();
+        return
+    }
+    if(!/[A-Z]/.test(password.value)){
+        errStore.loginErrorLog = "Password doesn't have any upper case"
+        errStore.loginFailed = true
+        event.preventDefault();
+        return
+    }
+    
+    if(!/[@#$%^&*!]/.test(password.value)){
+        errStore.loginErrorLog = "Password doesn't have any special character"
+        errStore.loginFailed = true
+        event.preventDefault();
+        return
+    }
     if(username.value.trim().length === 0 || password.value.trim().length === 0){
             errStore.loginErrorLog = "Please fulfill both username and password."
             errStore.loginFailed = true
@@ -53,14 +82,57 @@ const regist = (event)=>{
         errStore.loginErrorLog = "Password doesn't match."
         errStore.loginFailed = true
         event.preventDefault();
-        return
+        return 
     }
+    
+
     addUserData(username.value,password.value)
     event.preventDefault();
     registrationSuccess.value = true
     
     
 }
+
+const computedPasswordHasDigit = computed(() => {
+    if(/[0-9]/.test(password.value)) { 
+        hasDigit.value = true
+        return 'Password have number'
+    }
+    else {
+        hasDigit.value = false
+        return "Password doesn't have any digit"
+    }
+})
+const computedPasswordHasLower = computed(() => {
+    if(/[a-z]/.test(password.value)) { 
+        hasLower.value = true
+        return 'Password have lower case'
+    }
+    else {
+        hasLower.value = false
+        return "Password doesn't have any lower case"
+    }
+})
+const computedPasswordHasUpper = computed(() => {
+    if(/[A-Z]/.test(password.value)) { 
+        hasUpper.value = true
+        return 'Password have upper case'
+    }
+    else {
+        hasUpper.value = false
+        return "Password doesn't have any upper case"
+    }
+})
+const computedPasswordHasspecial = computed(() => {
+    if(/[@#$%^&*!]/.test(password.value)) { 
+        hasSpecial.value = true
+        return 'Password have special character'
+    }
+    else {
+        hasSpecial.value = false
+        return "Password doesn't have any special character"
+    }
+})
 
 const closeModal = ()=>{
     registrationSuccess.value = false
@@ -92,6 +164,16 @@ onMounted(async () => {
                 <input type="password" placeholder="Password" v-model="password" class="bg-slate-400 rounded-md h-10 text-black p-1" >
                 <label v-if="registerMode" class=" font-semibold divider divider-neutral divider-start font-basblue mb-0">Password Again</label>
                 <input v-if="registerMode" type="password" placeholder="Password" v-model="validatePassword" class="bg-slate-400 rounded-md h-10 text-black p-1" >
+                <div v-if="registerMode">
+                    <div v-if="hasDigit" class="text-green-500">{{computedPasswordHasDigit}}</div>
+                    <div v-else class="text-red-500">{{computedPasswordHasDigit}}</div>
+                    <div v-if="hasLower" class="text-green-500">{{computedPasswordHasLower}}</div>
+                    <div v-else class="text-red-500">{{computedPasswordHasLower}}</div>
+                    <div v-if="hasUpper" class="text-green-500">{{computedPasswordHasUpper}}</div>
+                    <div v-else class="text-red-500">{{computedPasswordHasUpper}}</div>
+                    <div v-if="hasSpecial" class="text-green-500">{{computedPasswordHasspecial}}</div>
+                    <div v-else class="text-red-500">{{computedPasswordHasspecial}}</div>
+                </div>
                 <div v-if="errStore.loginFailed" class="text-red-500">{{ errStore.loginErrorLog }}</div>
                 <input v-if="!registerMode" type="submit" class="btn bg-blue-500 text-slate-200 hover:bg-slate-200 hover:text-blue-500" value="Login" @click="login($event)" >
                 <input v-if="registerMode" type="submit" class="btn bg-blue-500 text-slate-200 hover:bg-slate-200 hover:text-blue-500" value="Sign up" @click="regist($event)" > 
